5b78d2c560b41cf41faf687716ff50bfaaba7a2f
Andrey <azenin010@gmail.com>
2022-03-20 10:55:04 +0400

    new tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -13,4 +13,39 @@ public class CashbackHackServiceTest {
         int expected = 1000;
         assertEquals(actual,expected);
     }
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(600);
+        int expected = 400;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain4() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain5() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain6() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual,expected);
+    }
 }
