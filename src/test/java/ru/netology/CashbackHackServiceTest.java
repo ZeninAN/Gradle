1497524f8759cfb78bd246f876449278611b0add
ZeninAN <95536097+ZeninAN@users.noreply.github.com>
2022-04-06 19:46:22 +0400

    broken test

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -30,7 +30,7 @@ public class CashbackHackServiceTest {
     public void testRemain4() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         Assert.assertEquals(expected, actual);
     }
     @Test
